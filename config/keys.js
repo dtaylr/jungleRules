6a1c7822b378e871a3656b2837ae5896e4fab202
gungopeas <silenta1moves@gmail.com>
2020-05-02 12:24:13 -0500

    forgot 1

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
-// module.exports = {
-//     mongoURI: 'mongodb+srv://ridah101:vsdzdcfdefsdxcersfer@animalsdb-6sptd.mongodb.net/test?retryWrites=true'
-// }
+
 module.exports = {
     mongoURI: `mongodb+srv://ridah101:${[process.env.REACT_APP_SERVY]}@animalsdb-6sptd.mongodb.net/test?retryWrites=true`
 }
